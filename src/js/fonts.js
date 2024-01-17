0dea000862ba05260c635f1e66ced46270a5aff4
Kaso <mackovision@gmail.com>
2024-01-17 15:58:37 -0800

    Beta testing new design, updated styles, added new fonts, new js to load fancy fonts

---
+++ src/js/fonts.js
@@ -0,0 +1,21 @@
+/**
+ * loadFonts - checks if custom fonts are loaded, then load all together in a promise.
+ * If user has  already done this, check session storage and skip this for subsequent pages.
+ */
+function loadFonts () {
+    if (sessionStorage.fontsLoaded) {
+        document.documentElement.classList.add('fonts-loaded');
+        return;
+    }
+
+    if ('fonts' in document) {
+        Promise.all([
+          document.fonts.load('1em Bungee Spice'),
+          document.fonts.load('1em Bungee Shade')
+        ]).then(_ => {
+          document.documentElement.classList.add('fonts-loaded')
+        });
+      }
+}
+
+loadFonts();
