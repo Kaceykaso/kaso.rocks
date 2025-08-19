939ae4e730328e12296ef9722f0919ab2208a040
Kaso <mackovision@gmail.com>
2025-08-18 23:39:47 -0700

    Hopefully fixing things

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
+import {HashRouter as Router} from 'react-router-dom';
 import { Hero } from './components/Hero';
 import { Gameboy } from './components/Gameboy';
 import { MessageList } from './components/MessageList';
 import { Footer } from './components/Footer';
 
-
 const App = () => {
 
   return (
-    <>
+    <Router>
       <Hero />
       <section className="main">
         <Gameboy />
         <MessageList />
       </section>
-
       <Footer />
-    </>
+    </Router>
   );
 };
 
